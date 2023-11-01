836807771974c329ba7dadba2dd7e64264fe9ceb
santiagomiras10 <134968642+santiagomiras10@users.noreply.github.com>
2023-11-01 17:05:55 -0300

    Add files via upload
    pagina principal con header y footer

--- index.php
+++ index.php
@@ -5,25 +5,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="icon" href="logo.jpg" type="logo.jpg">
     <title>Página principal</title>
     <style>
-        body{
-            background: linear-gradient(45deg, #289e92, #c22a2c);
-            background-repeat: no-repeat;
-            height: 100vh
-        }
-        h1{
-            color: antiquewhite;
-            font-style: oblique;
-        }
-        .btn-custom{
-            background-color: #c22a2c;
-            color: antiquewhite;
-        }
+
     </style>
 </head>
 <body>
+<header>
+        <div class="logo">
+            <img src="logo_muni.jpg" alt="">
+        </div>
+        <nav>
+            <ul>
+            <li><a href="login.php"><img src="cerrar_sesion.png"></a></li>
 
+            </ul>
+        </nav>
+    </header>
     <br>
     <h1 class="text-center">¡Bienvenido administrador!</h1>
     <br>
@@ -48,6 +48,20 @@
         </div>
     </div>
     </div>
+
+
+
+    <footer>
+        <div class="content-wrapper">
+        <h4>SEGUINOS EN</h4>
+        <div class="footer-images">
+            <a href="https://www.instagram.com/municipalidad.montecristo/?utm_source=ig_web_button_share_sheet&igshid=OGQ5ZDc2ODk2ZA=="><img src="ig_rojo.png" alt="Instagram"></a>
+            <a href="https://www.facebook.com/MunicipalidaddeMonteCristo"><img src="face_rojo.png" alt="Facebook"></a>
+        </div>
+
+        <h5>Luis Tagle 295, Monte Cristo, Cordoba Argentina</h5>
+        <h5>&copy;Municipalidad de Monte Cristo</h5>
+    </footer>
 
     
 
