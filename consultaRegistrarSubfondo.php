6220ba98a71199ec650065cd444d0490318310a3
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-11-01 17:24:59 -0300

    FORMULARIO REGISTRAR SUBFONDO
    son dos archivos je

---
+++ consultaRegistrarSubfondo.php
@@ -0,0 +1,25 @@
+<?php
+    include("conexion.php");
+
+//trae los datos del formulario
+
+    $nombre_subfondo = $_POST['nombre_subfondo'];
+    
+
+//inserta los datos
+
+    $sql = "INSERT INTO subfondo VALUES(NULL,'$nombre_subfondo')";
+    
+//si se pueden ingresar, deriva a la pagina principal
+
+    if ($datos_bd->query($sql) === TRUE) {
+        header("location:index.php");
+    } else {
+        echo "Error al insertar el subfondo: " . $datos_bd->error;
+    }
+
+//cierra la conexion
+    
+    $datos_bd->close();
+    ?>
+?>
