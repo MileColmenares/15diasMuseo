f1d61ede6d8fb1b090e740e1cfa951b91e76c8dc
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-11-13 23:47:45 -0300

    MOSTRAR FINAL

--- mostrar.php
+++ mostrar.php
@@ -18,45 +18,59 @@
     $result = $datos_bd->query($sql);
     if ($result->num_rows > 0) {
         echo "<div class'containerQ'>";
-        echo "<table class='tableA'>";
+        echo "<div class='table-responsive'>";
+        echo "<table class='table'>";
         echo "<thead>";
         echo "<tr>";
         echo "<th>Id</th>";
+        echo "<th>Fondo, subfondo, serie</th>";
         echo "<th>Nombre</th>";
-        echo "<th>Fechas</th>";
+        echo "<th>Fecha(s)</th>";
         echo "<th>Volumen y soporte</th>";
         echo "<th>Ubicación</th>";
         echo "<th>Nombre del productor</th>";
         echo "<th>Nombre del creador</th>";
-        echo "<th>Historia archivística</th>";
         echo "<th>Forma de ingreso</th>";
         echo "<th>Descripción</th>";
         echo "<th>Condición de acceso</th>";
         echo "<th>Condición de reproducción</th>";
+        echo "<th>Descripción de estado</th>";
+        echo "<th>Intervenciones y recomendaciones</th>";
+        echo "<th>Documentos relacionados</th>";
+        echo "<th>Notas</th>";
+        echo "<th>Responsables</th>";
         echo "</tr>";
         echo "</thead>";
         echo "<tbody>";
-        echo "</div>";
+        
+
 // Tu código para llenar las filas de la tabla
 while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo "<td data-label='Id'>" . $row['id_documento'] . "</td>";
+    echo "<td data-label='Fondo, subfondo, serie'>" . $row['id_fondo'] . "<br>" . $row['id_subfondo'] ."<br>" . $row['id_serie'] ."<br>" . $row['id_subserie'] . "</td>";
     echo "<td data-label='Nombre'>" . $row['nombre_documento'] . "</td>";
-    echo "<td data-label='Fechas'>" . $row['fecha_inicio'] . "<br>" . $row['fecha_final'] . "</td>";
+    echo "<td data-label='Fecha(s)'>" . $row['fecha_inicio'] . "<br>" . $row['fecha_final'] . "</td>";
     echo "<td data-label='Volumen y soporte'>" . $row['volumen_soporte'] . "</td>";
-    echo "<td data-label='Ubicación'>" . $row['id_ubicacion'] . "</td>";
+    echo "<td data-label='Ubicación'>" . $row['ubicacion'] . "</td>";
     echo "<td data-label='Nombre del productor'>" . $row['nombre_productor'] . "</td>";
     echo "<td data-label='Nombre del creador'>" . $row['nombre_creador'] . "</td>";
-    echo "<td data-label='Historia archivística'>" . $row['historia_archivistica'] . "</td>";
     echo "<td data-label='Forma de ingreso'>" . $row['forma_ingreso'] . "</td>";
     echo "<td data-label='Descripción'>" . $row['descripcion'] . "</td>";
     echo "<td data-label='Condición de acceso'>" . $row['condicion_acceso'] . "</td>";
     echo "<td data-label='Condición de reproducción'>" . $row['condicion_reproduccion'] . "</td>";
+    echo "<td data-label='Descripción de estado'>" . $row['descripcion_estado'] . "</td>";
+    echo "<td data-label='Intervenciones y recomendaciones'>" . $row['intervenciones'] . "<br>" . $row['recomendaciones'] . "</td>";
+    echo "<td data-label='Documentos relacionados'>" . $row['documentos_relacionados'] . "</td>";
+    echo "<td data-label='Notas'>" . $row['notas'] . "</td>";
+    echo "<td data-label='Responsables'>" . "Responsable del registro: " . $row['resp_registro'] . "<br> Responsable del control: " . $row['resp_control'] . "</td>";
+
     echo "</tr>";
 }
-echo "</tbody>";
-echo "</table>";
-echo "</div>";
+    echo "</tbody>";
+    echo "</table>";
+    echo "</div>";
+    echo "</div>";
 
     //echo "<td><a href='modificar.php?id=" . $row['id_documento'] . "'>Modificar</a></td>";
    // echo "<td><a href='eliminar.php?id=" . $row['id_documento'] . "'>Eliminar</a></td>";
