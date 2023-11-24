c7d1b4c45dae098ed57099cb9e907acce0a538aa
MileColmenares <132697364+MileColmenares@users.noreply.github.com>
2023-11-24 14:58:02 -0300

    MOSTRAR U_DOCUMENTAL/BUSCADOR

--- mostrar.php
+++ mostrar.php
@@ -1,20 +1,15 @@
 
 <head>
 <link rel="stylesheet" type="text/css" href="">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="PRUEBA.css">
-
+<link rel="stylesheet" type="text/css" href="css/PRUEBA.css">
 </head>
-
 
     <?php
     include("conexion.php");
-
-    
     include("header.php");
     include ("buscador.php");
     // obtener datos
-    $sql = "SELECT * FROM unidad_documental WHERE id_documento";
+    $sql = "SELECT * FROM unidad_documental1 WHERE id_documento";
     $result = $datos_bd->query($sql);
     if ($result->num_rows > 0) {
         echo "<div class'containerQ'>";
@@ -23,47 +18,28 @@
         echo "<thead>";
         echo "<tr>";
         echo "<th>Id</th>";
-        echo "<th>Fondo, subfondo, serie</th>";
+        echo "<th>Fondo, subfondo, serie, subserie</th>";
         echo "<th>Nombre</th>";
         echo "<th>Fecha(s)</th>";
-        echo "<th>Volumen y soporte</th>";
         echo "<th>Ubicación</th>";
-        echo "<th>Nombre del productor</th>";
-        echo "<th>Nombre del creador</th>";
-        echo "<th>Forma de ingreso</th>";
-        echo "<th>Descripción</th>";
         echo "<th>Condición de acceso</th>";
         echo "<th>Condición de reproducción</th>";
-        echo "<th>Descripción de estado</th>";
-        echo "<th>Intervenciones y recomendaciones</th>";
-        echo "<th>Documentos relacionados</th>";
-        echo "<th>Notas</th>";
-        echo "<th>Responsables</th>";
+        echo "<th>Ver mas</th>";
         echo "</tr>";
         echo "</thead>";
         echo "<tbody>";
-        
 
 // Tu código para llenar las filas de la tabla
 while ($row = $result->fetch_assoc()) {
-    echo "<tr>";
+    
     echo "<td data-label='Id'>" . $row['id_documento'] . "</td>";
     echo "<td data-label='Fondo, subfondo, serie'>" . $row['id_fondo'] . "<br>" . $row['id_subfondo'] ."<br>" . $row['id_serie'] ."<br>" . $row['id_subserie'] . "</td>";
     echo "<td data-label='Nombre'>" . $row['nombre_documento'] . "</td>";
     echo "<td data-label='Fecha(s)'>" . $row['fecha_inicio'] . "<br>" . $row['fecha_final'] . "</td>";
-    echo "<td data-label='Volumen y soporte'>" . $row['volumen_soporte'] . "</td>";
     echo "<td data-label='Ubicación'>" . $row['ubicacion'] . "</td>";
-    echo "<td data-label='Nombre del productor'>" . $row['nombre_productor'] . "</td>";
-    echo "<td data-label='Nombre del creador'>" . $row['nombre_creador'] . "</td>";
-    echo "<td data-label='Forma de ingreso'>" . $row['forma_ingreso'] . "</td>";
-    echo "<td data-label='Descripción'>" . $row['descripcion'] . "</td>";
     echo "<td data-label='Condición de acceso'>" . $row['condicion_acceso'] . "</td>";
     echo "<td data-label='Condición de reproducción'>" . $row['condicion_reproduccion'] . "</td>";
-    echo "<td data-label='Descripción de estado'>" . $row['descripcion_estado'] . "</td>";
-    echo "<td data-label='Intervenciones y recomendaciones'>" . $row['intervenciones'] . "<br>" . $row['recomendaciones'] . "</td>";
-    echo "<td data-label='Documentos relacionados'>" . $row['documentos_relacionados'] . "</td>";
-    echo "<td data-label='Notas'>" . $row['notas'] . "</td>";
-    echo "<td data-label='Responsables'>" . "Responsable del registro: " . $row['resp_registro'] . "<br> Responsable del control: " . $row['resp_control'] . "</td>";
+    echo "<td><a href='mostrarMas.php?id=" . $row['id_documento'] . "'>Ver Mas</a></td>";
 
     echo "</tr>";
 }
